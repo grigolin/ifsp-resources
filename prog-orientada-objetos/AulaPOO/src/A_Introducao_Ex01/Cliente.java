0af47d7f6e52ce9d8377845b9ed82b924065c279
grigolin <68489223+grigolin@users.noreply.github.com>
2023-08-01 12:01:23 -0300

    Add files via upload

---
+++ prog-orientada-objetos/AulaPOO/src/A_Introducao_Ex01/Cliente.java
@@ -0,0 +1,18 @@
+
+package A_Introducao_Ex01;
+/**
+ *
+ * @author Aluno
+ */
+public class Cliente {
+    private String nome, rg;
+    
+    public void mostrar(String nome, String rg){
+    this.nome = nome;
+    this.rg;
+    System.out.printls("O nome foi: " + this.nome)
+            
+    }
+    
+    public void MostrarNome(String nome)
+}
