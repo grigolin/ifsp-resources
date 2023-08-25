e274c967de96e1cf82c67fe8d4afd8852d242aab
grigolin <68489223+grigolin@users.noreply.github.com>
2023-08-25 12:04:33 -0300

    Add files via upload

---
+++ estrutura-de-dados/lista-exercicios/lista-struct/StructwBubbleSort/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: veterano
+ *
+ * Created on 25 de agosto de 2023, 09:04
+ */
+
+#include <cstdlib>
+
+#include "cAluno.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cAluno obj;
+    obj.ReceberDados();
+    return 0;
+}
+
